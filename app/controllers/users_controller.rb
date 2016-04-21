bef6dc5f7437e13694e51f034662c65a0c61e127
Yahya Hussein <yahyahussein91@gmail.com>
2016-04-21 16:25:12 -0700

    fixed pw confirm validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[user_params])
+  	@user = User.new(user_params)
   	if @user.save
   		# Handle a successful save.
   	else
