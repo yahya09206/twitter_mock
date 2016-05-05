4316e76d7cff41fe7c875a2ca9e250a8b8b46031
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 21:46:53 -0700

    finished with valid test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -23,5 +23,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	follow_redirect!
   	assert_template 'users/show'
   	assert_select 'a[href=?]', login_path, count: 0
-  	
+  	assert_select 'a[href=?]', logout_path
+  	assert_select 'a[href=?]', user_path(@user)
+
 end
