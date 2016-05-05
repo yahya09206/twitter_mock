c0acbd2872ec4355768dbcb7b955d42a7a2e7800
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 23:54:55 -0700

    test for right user returned

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -5,3 +5,9 @@ class SessionsHelperTest < ActionView::TestCase
 	def setup
 		@user = users(:malik)
 		remember(@user)
+	end
+
+	test 'current_user returns right user when session is nil' do 
+		assert_equal @user, current_user
+		assert is_logged_in
+	end
