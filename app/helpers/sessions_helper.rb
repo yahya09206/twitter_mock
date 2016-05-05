a14ecec2fc2d6c437e8a4649259ce24663f25f17
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 23:00:51 -0700

    finished remembering user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,11 @@ module SessionsHelper
 	end
 
 	#Remembers a user in a persistent session
-	
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
 
 	#returns the current logged-in user (if any)
 	def current_user
