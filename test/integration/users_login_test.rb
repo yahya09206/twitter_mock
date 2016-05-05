d3cb62f04dfc9aa3e0592021ef44eff81e69ba5c
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 22:08:25 -0700

    finished with logout test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -31,6 +31,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     follow_redirect!
     assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 
 end
