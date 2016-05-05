b3c02cc211c97b4656d16f98ab30c7e0674a98fa
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 23:11:10 -0700

    finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,12 @@ end
 		!current_user.nil?
 	end
 
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	def log_out
 		forget(current_user)
 		session.delete(:user_id)
