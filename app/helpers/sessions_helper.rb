012e0ca061d40702585d0dc2d3fa00c5af41f0a3
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 22:59:47 -0700

    remembering the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,9 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	#Remembers a user in a persistent session
+	
+
 	#returns the current logged-in user (if any)
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
