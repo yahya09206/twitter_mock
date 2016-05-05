af473fcc9e11e22511a228d520002f2c9c58461c
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-04 23:05:13 -0700

    updating current user for persistent sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,12 @@ module SessionsHelper
 
 	#returns the current logged-in user (if any)
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user &&
+			
 	end
 
 	def logged_in?
