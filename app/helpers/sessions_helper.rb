e9b29636ff1b94381c2ed50797190ef83bd9353d
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:57:30 -0700

    code for stored local

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,8 @@ end
 	end
 
 	# Redirects to stored location (or to the default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
 end
