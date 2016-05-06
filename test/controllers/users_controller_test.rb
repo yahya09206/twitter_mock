bf7813d3f68c374419e0b1c08d9461d88e621b59
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:17:29 -0700

    test for wrong user update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,6 +24,14 @@ class UsersControllerTest < ActionController::TestCase
   	assert_redirected_to login_url
   end
 
-  test 'should redirect edit when logged in as wrong user'
+  test 'should redirect edit when logged in as wrong user' do 
+  	log_in_as(@other_user)
+  	get :edit, id:@user
+  	assert flash.empty?
+  	assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do 
+
 
 end
