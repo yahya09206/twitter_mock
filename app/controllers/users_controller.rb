bbc3442da3b10f4316182148810f085038c5bf14
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:02:38 -0700

    finished before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,4 +38,11 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url   
+      end
+    end
 end
