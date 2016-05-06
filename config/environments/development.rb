31359ec7c28805d5a31a9393bac6db95c726d59a
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-06 16:52:57 -0700

    fixed development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_metho = :test
+  config.action_mailer.delivery_method = :test
   host = 'example.com'
   config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'https'}
 
