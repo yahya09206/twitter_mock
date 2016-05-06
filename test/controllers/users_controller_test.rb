c0ac38db2f911629fd96b795dc0bfdefb8ce4832
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:09:24 -0700

    start test so edit/update are protected

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+	def setup
+		@user = users(:malik)
+	end
+
+	
   test "should get new" do
     get :new
     assert_response :success
