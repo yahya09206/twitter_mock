c0bca77a0993ac644376a100f7b7801a01b21a0c
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:58:28 -0700

    code for stored url

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -51,4 +51,9 @@ end
 		redirect_to(session[:forwarding_url] || default)
 		session.delete(:forwarding_url)
 	end
+
+	# Stores the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
 end
