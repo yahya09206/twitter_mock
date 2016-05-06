40fe1c6c675cfdb9d8e5bb4d3c652acb670870d4
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 18:43:15 -0700

    finished integration test for unsuccessful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = user(:malik)
+  end
+
+  test 'unsuccessful edit' do 
+  	get edit_user_path(@user)
+  	assert_template 'users/edit'
+  	path user_path(@user), user: {
+  		name: '',
+  		email: 'foo@invalid',
+  		password: 'foo',
+  		password_confirmation: 'bar'
+  	}
+
+  	assert_template 'users/edit'
+  end
 end
