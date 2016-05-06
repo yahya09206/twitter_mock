66966b8388cc51aa223d7450b5a83a4c4fefc35d
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 19:10:26 -0700

    redirect to edit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,10 +6,16 @@ class UsersControllerTest < ActionController::TestCase
 		@user = users(:malik)
 	end
 
-	
+
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test 'should redirect edit when not logged in' do 
+  	get :edit, id: @user
+  	assert_not flash.empty?
+  	assert_redirected_to login_url
   end
 
 end
