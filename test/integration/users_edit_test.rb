62304cdfcbc1f0affe2777dcd67e6aa7cf19528d
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-05 20:04:39 -0700

    fixed test to pass

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -44,10 +44,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	assert_redirected_to edit_user_path(@user)
   	name = 'Foo Bar'
   	email = 'foo@bar.com'
-  	patch user_path(@usr), user: {
+  	patch user_path(@user), user: {
   		name: name, 
   		email: email,
-  		password: ''
+  		password: '',
   		password_confirmation: ''
   	}
   	assert_not flash.empty?
