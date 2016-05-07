7751ad7d3ec8fd0df889584401ee07a840606bd9
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-07 16:58:13 -0700

    finished with signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -41,7 +41,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
      get edit_account_activation_path(user.activation_token, email: 'wrong')
      assert_not is_logged_in?
      # Valid activation token
-     
+     get edit_account_activation_path(user.activation_token, email: user.email)
+     assert user.reload.activated?
+     follow_redirect!
   	 assert_template 'users/show'
      assert is_logged_in?
   	end
