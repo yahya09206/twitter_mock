d27128a8cf09941fde89975ac3854b2850f6c20b
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-07 16:49:53 -0700

    added end statement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
         message += "Check your email for the activation link."
         flash[:warning] = message
         redirect_to root_url
+      end
   		# Log the user in and redirect to the user's show page.
   	else
   		# Create an error message.
