f55565a7ccdf8edd056730ee9ad50c3d4624f85c
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-06 19:36:47 -0700

    fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,10 +35,10 @@ class User < ActiveRecord::Base
 	end
 
 	#Returns true if the given token matches the digest.
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
-	end
+	# def authenticated?(remember_token)
+	# 	return false if remember_digest.nil?
+	# 	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	# end
 
 	#Forgets a user
 	def forget
