15817279f0fe2b7013693921a6f303c9f801ebd4
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-07 16:40:50 -0700

    finished with account activatin edit

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -7,4 +7,10 @@ class AccountActivationsController < ApplicationController
 			user.update_attribute(:activated_at, Time.zone.now)
 			log_in user
 			flash[:success] = 'Account activated!'
+			redirect_to user
+		else
+			flash[:danger] = 'Invalid activation link'
+			redirect_to root_url
+		end 
+	end
 end
