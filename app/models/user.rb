a7370ae26e9f35c5b99c0f9c32684e8a22d2528a
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-07 19:26:00 -0700

    finished pw reset methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,14 @@ class User < ActiveRecord::Base
 	# Sets the password reset attributes.
 	def create_reset_digest
 		self.reset_token = User.new_token
-		
+		update_attribute[:reset_digest, User.digest(reset_token)]
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# Sends password reset email.
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
 
 	private
 
