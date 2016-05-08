4fe1714a1657dd81183d92f391008e473ceefeb1
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-07 17:00:28 -0700

    fixed account activation controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,7 @@
 class AccountActivationsController < ApplicationController
 
 	def edit
-		user = User.edit.find_by(email: params[:email])
+		user = User.find_by(email: params[:email])
 		if user && !user.activated? && user.authenticated?(:activation, params[:id])
 			user.update_attribute(:activated, true)
 			user.update_attribute(:activated_at, Time.zone.now)
