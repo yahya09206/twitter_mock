977308eeb93b423db8012f4324a7901b23eeb2d8
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-08 18:09:24 -0700

    finish with private meths

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,5 +21,16 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
-  
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms valid user
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
