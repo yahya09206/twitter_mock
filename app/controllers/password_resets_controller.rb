b467fdde4ea020dab512b57ef2755d0755dd0b15
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-08 18:16:01 -0700

    almost done pw update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -22,7 +22,15 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
-  
+  def update 
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset"
+      
+        
 
   private
 
