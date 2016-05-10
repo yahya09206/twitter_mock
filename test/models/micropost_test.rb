690e8a371ed785e7007bfcc6fd630c20c695069d
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-09 23:23:47 -0700

    last fix

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   
   def setup
-  	@user = users[:malik]
+  	@user = users(:malik)
   	#this code is not idiomatically correct.
   	@micropost = Micropost.new(content: 'Lorem ipsum', user_id: @user.id)
   end
