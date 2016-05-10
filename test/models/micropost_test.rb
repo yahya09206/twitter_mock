c54031c1024e8cd7a4e65bf506eca49b826a9ad2
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-09 23:18:11 -0700

    finish test for valid post

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -7,4 +7,9 @@ class MicropostTest < ActiveSupport::TestCase
   	#this code is not idiomatically correct.
   	@micropost = Micropost.new(content: 'Lorem ipsum', user_id: @user.id)
   end
+
+  test 'should be valid' do 
+  	assert @micropost.user_id = nil
+  	assert_not @micropost.valid?
+  end
 end
