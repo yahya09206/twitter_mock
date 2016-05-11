d41c0a54b2b0077a76795b8d74e2cd612436d685
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-11 16:48:19 -0700

    finish destroy test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,6 +66,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
-  test ''
+  test 'associated microposts should be destroyed' do 
+    @user.save
+    @user.microposts.create!(content: 'Lorem ipsum')
+    assert_difference 'Micropost.count', -1 do 
+      @user.destroy 
+    end
+  end
 
 end
