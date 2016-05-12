023dafa43436e84debe6a13b67d4ae001c604785
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-11 21:37:05 -0700

    add post to sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,8 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+
+users = User.order(:created_at).take(6)
+50.times do 
+  content = Faker::Lorem.sentence(5)
 end
