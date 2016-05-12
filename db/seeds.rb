0da6da0aa372f6bc9f54a411fb3e5239fe1c73ed
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-11 21:47:05 -0700

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,5 +29,5 @@ end
 users = User.order(:created_at).take(6)
 50.times do 
   content = Faker::Lorem.sentence(5)
-  users.each { |user| user.micrposts.create!(content: content)}
+  users.each { |user| user.micrposts.create!(content: content) }
 end
