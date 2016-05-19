4e0ac5c1b601ab15ffeb6670e6993efafac8ec12
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 14:34:10 -0700

    start of profile display

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,5 +1,12 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  
+  include ApplicationHelper
+
+  def setup
+  	@user = users(:malik)
+  end
+
+  test 'profile display' do
+
 end
