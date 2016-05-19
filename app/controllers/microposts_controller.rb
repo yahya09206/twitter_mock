67097a1e30095d7bb00a76a5b8343f5310b6a12a
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 15:04:42 -0700

    create action for posts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,9 +2,10 @@ class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 
 	def create
+		@micropost = current_user.micrposts.build(micropost_params)
 	end
 
 	def destroy
 	end
-	
+
 end
