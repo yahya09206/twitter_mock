82cee1859fcd665c51a853dd99cef5c1b8a8658e
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 14:55:26 -0700

    finish w/ create test

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -5,4 +5,13 @@ class MicropostsControllerTest < ActionController::TestCase
   def setup
   	@micropost = microposts(:orange)
   end
+
+  test 'should redirect create when not logged in' do 
+  	assert_no_difference 'Micropost.count' do 
+  		post :create, micropost: { content: 'Lorem ipsum'}
+  	end
+  	assert_redirected_to login_url
+  end
+
+  
 end
