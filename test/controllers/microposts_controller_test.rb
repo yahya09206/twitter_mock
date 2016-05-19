07ea26ac26d3699d1907216f75056e3a3f6a65e9
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 14:53:45 -0700

    auth test for micropost cont

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@micropost = microposts(:orange)
+  end
 end
