8ac15145bb2b78f8c94c69485f1740cacec40f72
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 16:03:46 -0700

    halway done with interface test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,4 +9,15 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   	log_in_as(@user)
   	get root_path
   	assert_select 'div.pagination'
+  	# Invalid submission
+  	assert_no_difference 'Micropost.count' do 
+  		post microposts_path, micropost: { content: ''}
+  	end
+  	assert_select 'div#error_explanation'
+  	#Valid submission
+  	content = 'This micropost really ties the room together'
+  	assert_difference 'Micropost.count', 1 do 
+  		post microposts_path, micropost: { content: content}
+  	end
+  	
 end
