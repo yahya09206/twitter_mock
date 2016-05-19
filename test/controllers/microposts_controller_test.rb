45d27c11eb53099d72db87a7320f518558b80318
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-19 14:56:33 -0700

    finish w destroy test

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -13,5 +13,11 @@ class MicropostsControllerTest < ActionController::TestCase
   	assert_redirected_to login_url
   end
 
+  test 'should redirect destroy when not logged in' do
+  	assert_no_difference 'Micropost.count' do 
+  		delete :destroy, id: @micropost
+  	end
+  	assert_redirected_to login_url
+  end
   
 end
