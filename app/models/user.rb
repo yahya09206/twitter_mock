f98fb4482457191bf734b79d63491581943475f5
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-21 19:31:22 -0700

    finished with following methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,6 +90,11 @@ class User < ActiveRecord::Base
 		active_relationships.find_by(followed_id: other_user.id).destroy
 	end
 
+	#Returns true if the current user is following the other user
+	def following?(other_user)
+		following.include?(other_user)
+	end
+
 	private
 
 		# Converts email to all lower-case
