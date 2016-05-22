57f43ed2148bb6650191855b89d5011571744064
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-21 19:18:21 -0700

    finish valid test

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -2,4 +2,12 @@ require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
   
+  def setup
+  	@relationship = Relationship.new(follower_id: 1, followed_id: 2)
+  end
+
+  test 'should be valid' do 
+  	assert @relationship.valid?
+  end
+
 end
