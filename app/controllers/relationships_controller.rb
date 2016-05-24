6b957f86b8bb488f90e56938c17fe98d8350fa28
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 21:11:27 -0700

    added ajax to destroy action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,7 +13,10 @@ class RelationshipsController < ApplicationController
 	def destroy
 		user = Relationship.find(params[:id]).followed 
 		current_user.unfollow(user)
-		redirect_to user
+		respond_to do |format|
+			format.html { redirect_to @user }
+			format.js
+		end
 	end
 	
 
