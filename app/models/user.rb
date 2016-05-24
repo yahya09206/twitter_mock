3160ea69c8feed5d225a0f3926af23eb776ae96d
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 22:03:00 -0700

    done with implementation of feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,7 +79,8 @@ class User < ActiveRecord::Base
 	# Defines a proto-feed.
 	# see 'Following users' for the full implementation
 	def feed
-		Micropost.where('user_id IN (:following_ids) OR user_id = :user_id', following_ids: following_ids, user_id: id)
+		following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+		Micropost.where("user_id IN ({following_ids}) OR user_id = :user_id", user_id: id)
 	end
 
 	# follows a user
