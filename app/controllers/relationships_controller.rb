abc930492d042e27e5658507d78ee36b31ce2a52
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 21:10:45 -0700

    added ajax to create action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,10 @@ class RelationshipsController < ApplicationController
 	def create
 		user = User.find(params[:followed_id])
 		current_user.follow(user)
-		redirect_to user
+		respond_to do |format|
+			format.html { redirect_to @user }
+			format.js
+		end
 	end
 
 	def destroy
