25ec7a7d5a1d1e5c642a548988b9c6beabe83eba
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 19:06:36 -0700

    finished with followers action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,14 @@ class UsersController < ApplicationController
     @title = 'Following'
     @user = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
-    render 'show/follow'
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
