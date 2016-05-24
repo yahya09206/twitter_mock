c0cdd8bf832d4bd3e9ab324460d91dc391e7327e
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 18:33:23 -0700

    finish with auth for follow page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,5 +59,8 @@ class UsersControllerTest < ActionController::TestCase
   		assert_redirected_to root_url
   	end
 
-    test 
+    test 'should redirect following when not logged in' do 
+      get :following, id:@user
+      assert_redirected_to login_url
+    end
 end
