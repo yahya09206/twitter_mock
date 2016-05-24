47ca640f52cc7b8e6d70c71f28f991e4be337cb7
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-23 18:52:21 -0700

    start for following page test

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 
 class FollowingTest < ActionDispatch::IntegrationTest
   
+  def setup
+  	@user = user(:malik)
+  	log_in_as(@user)
+  end
+
+  test 'following page' do
+
 end
