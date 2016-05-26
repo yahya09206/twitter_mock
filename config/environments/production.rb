766678cfcd840e51a8e1e78878dc0c55440b8eb6
Yahya Hussein <yahyahussein91@gmail.com>
2016-05-26 15:06:08 -0700

    finished with settings for activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls => true
   }
 
   # Disable serving static files from the `/public` folder by default since
